5a51503743edb86bb9c2d7dcbdcee7e4dc1391a0
Sumit Vikram <vikramsumit@outlook.com>
2025-04-12 17:55:06 +0530

    first commit

---
+++ backtracking/a.java
@@ -0,0 +1,21 @@
+package backtracking;
+
+public class a {
+    public static void changeArr(int arr[], int i , int val) {
+        //base case
+
+        //recursion
+    }
+
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        changeArr(arr, 0, 1git );
+        printArr(arr);
+    }
+}
