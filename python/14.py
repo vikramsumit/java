a05c52ad6c55c61e6fa08d3a4280c3750f517cd5
sumit vikram <vikramsumit@outlook.com>
2025-12-02 22:30:03 +0530

    Initial commit

--- python/14.py
+++ python/14.py
@@ -1,12 +1,3 @@
-#
-# Complete the 'print_full_name' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. STRING first
-#  2. STRING last
-#
-
 def print_full_name(first_name, last_name):
     print(f"Hello {first_name} {last_name}! You just delved into python.")
 
