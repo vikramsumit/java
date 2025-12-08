dc26c8dec71449b830d330bf3ebd85a05b9bbc43
sumit vikram <vikramsumit@outlook.com>
2025-12-08 19:34:25 +0530

    Initial commit

--- python/23.py
+++ python/23.py
@@ -1,7 +1,7 @@
 import textwrap
 
 def wrap(string, max_width):
-    return 
+    return textwrap.fill(string, width = max_width())
 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
